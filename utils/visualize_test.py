d28570ca3e395b3715e9c4e5d295948412d312e6
BestNaonao <3375579162@qq.com>
2026-01-10 02:59:25 +0800

    添加切分文档的可视化测试

---
+++ utils/visualize_test.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+import torch
+from dotenv import find_dotenv, load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_milvus import Milvus
+from transformers import AutoTokenizer
+from utils import MarkdownTreeParser, encode_document_for_milvus, decode_document_from_milvus
+from utils.chunker_utils import visualize_document_tree
+
+load_dotenv(find_dotenv())
+MILVUS_HOST = os.getenv('MILVUS_HOST')
+MILVUS_PORT = os.getenv('MILVUS_PORT')
+MILVUS_USER = os.getenv('MILVUS_USER')
+MILVUS_PASSWORD = os.getenv('MILVUS_ROOT_PASSWORD')
+
+RAW_DATA_DIR = "../raw_data"
+
+qwen_path = "../models/Qwen/Qwen3-Embedding-0.6B"
+
+# 1. 初始化嵌入模型
+embeddings = HuggingFaceEmbeddings(
+    model_name=qwen_path,
+    model_kwargs={"device": "cuda", "trust_remote_code": True},
+    encode_kwargs={"normalize_embeddings": True}
+)
+
+# 2. 初始化 MarkdownTreeParser
+tokenizer = AutoTokenizer.from_pretrained(qwen_path, trust_remote_code=True)
+parser = MarkdownTreeParser(
+    embeddings=embeddings,
+    tokenizer=tokenizer,
+    min_chunk_size=256,
+    core_chunk_size=512,
+    max_chunk_size=1024
+)
+
+# 3. 解析所有 Markdown 文件
+docs = []
+for file in ["../raw_data/文档_任务_管理集群_IP Masquerade Agent 用户指南.md"]:
+    print(f"正在处理：{file}, 已完成：{len(docs)}")
+    if file.endswith(".md"):
+        file_path = os.path.join(RAW_DATA_DIR, file)
+        docs.extend(parser.parse_markdown_to_tree(file_path))
+        torch.cuda.empty_cache()
+
+visualize_document_tree(docs, show_siblings=True)
