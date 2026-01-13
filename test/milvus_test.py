7b9a48051c0199801fabb600378f90b16f1b2cd0
BestNaonao <3375579162@qq.com>
2026-01-13 21:28:49 +0800

    将milvus_kb_test迁移到milvus_test

--- test/milvus_test.py
+++ test/milvus_test.py
@@ -1,11 +1,11 @@
 import os
 
+from dotenv import load_dotenv, find_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_milvus import Milvus
-from dotenv import load_dotenv, find_dotenv
 from pymilvus import Collection, connections
-from transformers import AutoConfig
 
+from utils.rag_utils import get_full_node_content
 
 # ==================== 环境变量加载 ====================
 load_dotenv(find_dotenv())
@@ -14,25 +14,18 @@ MILVUS_PORT = os.getenv('MILVUS_PORT')
 MILVUS_USER = os.getenv('MILVUS_USER')
 MILVUS_PASSWORD = os.getenv('MILVUS_ROOT_PASSWORD')
 
-def main_test(embedding_model_path, collection_name):
-    # 测试加载QWen的配置
-    config = AutoConfig.from_pretrained(embedding_model_path, trust_remote_code=True)
-    print(config.model_type)  # 如输出 "qwen3"
-
-    # 初始化嵌入模型
+def milvus_store(embedding_model_path, collection_name, index_params=None, search_params=None) -> Milvus:
     embedding_model = HuggingFaceEmbeddings(
         model_name=embedding_model_path,
         model_kwargs={
-            "device": "cuda",               # 如果无 GPU，改为 "cpu"
-            "trust_remote_code": True,      # Qwen 必须开启
+            "device": "cuda",  # 如果无 GPU，改为 "cpu"
+            "trust_remote_code": True,  # Qwen 必须开启
         },
         encode_kwargs={
-            "normalize_embeddings": True    # Qwen 推荐开启，用于 COSINE 相似度
+            "normalize_embeddings": True  # Qwen 推荐开启，用于 COSINE 相似度
         }
     )
-
-    # 连接到 Milvus
-    vector_store = Milvus(
+    return Milvus(
         embedding_function=embedding_model,
         connection_args={
             "host": MILVUS_HOST,
@@ -41,6 +34,15 @@ def main_test(embedding_model_path, collection_name):
             "password": MILVUS_PASSWORD,
         },
         collection_name=collection_name,
+        index_params=index_params,
+        search_params=search_params,
+    )
+
+def basic_test(embedding_model_path, collection_name):
+    # 连接到 Milvus
+    vector_store = milvus_store(
+        embedding_model_path,
+        collection_name,
         # 注意：index_params 在 add_texts 首次调用时才会生效（如果集合不存在）
         index_params={
             "index_type": "HNSW",
@@ -105,8 +107,64 @@ def main_test(embedding_model_path, collection_name):
     # 刷新
     collection.flush()
 
+def kb_test(embedding_model_path, collection_name):
+    vector_store = milvus_store(embedding_model_path, collection_name)
+
+    def condition_query(expr: str):
+        docs = vector_store.search_by_metadata(
+            expr=expr,
+            limit=500
+        )
+        print(f"共 {len(docs)} 条")
+        for doc in docs[:10]:
+            print(doc.metadata['title'])
+        return docs
+
+    print("=========“接下来”和“另请参见”导航章节测试")
+    condition_query("text like '%# 接下来%'")
+    condition_query("text like '%# 另请参见%'")
+
+    print("=========API块测试")
+
+    condition_1 = "(text like '%# HTTP%')"
+    condition_2 = "(text like '%# 参数%' or text like '%# Parameter%')"
+    condition_3 = "(text like '%# 响应%' or text like '%# Response%')"
+
+    condition_4 = "(text like '%**HTTP%')"
+    condition_5 = "(text like '%**参数**%' or text like '%**Parameter%')"
+    condition_6 = "(text like '%**响应**%' or text like '%**Response%')"
+
+    condition_7 = "(title like '文档_参考_Kubernetes API%')"
+
+    def satisfy_2_at_least(c1, c2, c3):
+        return f"(({c1} and {c2}) or ({c2} and {c3}) or ({c3} and {c1}))"
+
+    satisfy_1_2_3_al2 = satisfy_2_at_least(condition_1, condition_2, condition_3)
+    satisfy_4_5_6_al2 = satisfy_2_at_least(condition_4, condition_5, condition_6)
+
+    def satisfy_only_one(c1, c2, c3):
+        return f"(({c1} and !{c2} and !{c3}) or (!{c1} and {c2} and !{c3}) or (!{c1} and !{c2} and {c3}))"
+
+    print("---------至少满足条件1、2、3中的两个")
+    condition_query(f"{satisfy_1_2_3_al2}")
+    print("---------只满足条件1、2、3中的一个，并且满足条件7")
+    condition_query(f"{satisfy_only_one(condition_1, condition_2, condition_3)} and {condition_7}")
+    print("---------至少满足条件4、5、6中的两个")
+    condition_query(f"{satisfy_4_5_6_al2}")
+    print("---------至少满足条件4、5、6中的两个，并且不满足条件7")
+    condition_query(f"{satisfy_4_5_6_al2} and !{condition_7}")
+    print("---------只满足条件4、5、6中的一个，并且满足条件7")
+    condition_query(f"{satisfy_only_one(condition_4, condition_5, condition_6)} and {condition_7}")
+    print("---------只满足条件4、5、6中的一个，并且不满足条件7")
+    condition_query(f"{satisfy_only_one(condition_4, condition_5, condition_6)} and !{condition_7}")
+
+def rag_utils_test(embedding_model_path, collection_name):
+    vector_store = milvus_store(embedding_model_path, collection_name)
+    print(get_full_node_content(vector_store, "9a0c26a5-decf-5184-b5fc-9a2e7fce0cd6"))
+
 
 if __name__ == "__main__":
     embedding = "../models/Qwen/Qwen3-Embedding-0.6B"   # 相对路径：从当前脚本所在目录出发
-    my_collection = "my_collection"
-    main_test(embedding, my_collection)
+    # basic_test(embedding, "my_collection")
+    # kb_test(embedding, "knowledge_base_v1")
+    rag_utils_test(embedding, "knowledge_base_v1")
